bc66fce82521e6c4d3c0ffc5a734665267624155
Ricardo da Rocha <ricardodarocha@outlook.com>
2024-04-23 14:06:22 -0300

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ const whenExternalScripts = (items = []) =>
 
 export default defineConfig({
   output: 'static',
-  site: 'https://ricardodarocha.github.io',
+  site: 'https://ricardodarocha.github.io/',
   base: '/4g/',
 
   integrations: [
